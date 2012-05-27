0e77c49ef33aaabfe600a84336689ce8aca0f194
Derek Gathright <drgath@gmail.com>
2012-05-26 21:22:18 -0700

    OO refactor

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 
 var cli = require('cli'),
     path = require('path'),
-    burt = require('../lib/burt'),
+    Burt = require('../lib/burt'),
     meta = require(path.join('../package.json'));
 
 cli.parse({
@@ -13,12 +13,24 @@ cli.parse({
 });
 
 cli.main(function () {
-    var opts = this.options;
+    var opts = this.options,
+        css;
 
     if (opts.version) {
         console.log('v' + meta.version);
         return;
     }
 
-    burt.toCSS(opts);
+    if (0) {
+        css = Burt.getCSS(opts.file);
+    }
+    else {
+
+        var reynolds = new Burt.Reynolds(opts.file);
+        css = reynolds.getCSS();
+
+        // console.log(reynolds.data);
+        // console.log(reynolds.template);
+    }
+    console.log(css);
 });
